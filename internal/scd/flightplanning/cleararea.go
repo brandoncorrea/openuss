500bf7dff93049eec57a2e05008d0bdd0da6648d
Brandon Correa <me@bwawan.com>
2026-08-11 20:38:54 -0400

    Remove redundant WriteHeader invocation

--- internal/scd/flightplanning/cleararea.go
+++ internal/scd/flightplanning/cleararea.go
@@ -7,7 +7,6 @@ import (
 )
 
 func ClearArea(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	util.WriteJSON(w, map[string]any{
 		"outcome": map[string]any{
 			"success": true,
