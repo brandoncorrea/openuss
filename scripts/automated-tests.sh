9310277987e72044ded95c62a2b84d2e4741b3f2
Brandon Correa <me@bwawan.com>
2026-09-05 22:12:40 -0400

    fix automated-tests script in ci

--- scripts/automated-tests.sh
+++ scripts/automated-tests.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 CONFIG_NAME="openuss_f3548"
 
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
 cp "./test/config/$CONFIG_NAME.yaml" "$INTERUSS_MONITORING_DIR/monitoring/uss_qualifier/configurations/dev/"
 
 cd $INTERUSS_MONITORING_DIR
