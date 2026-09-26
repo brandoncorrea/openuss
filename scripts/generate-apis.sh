baf89ed507fccb2a945ffa1caa1c62ba74b9e05c
Brandon Correa <me@bwawan.com>
2026-09-26 14:06:27 -0500

    chore: bump dss ref in openapi generation

--- scripts/generate-apis.sh
+++ scripts/generate-apis.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 REPO_ROOT="$(pwd)"
 
-DSS_REF="277a9d6432fe2ab0fdb3046ea6020aa379de4b57"
+DSS_REF="ee6a93ce45d46c502f644e8dbf3c358c9c369f0f"
 GENERATOR_IMAGE="openuss/openapi-to-go-server:${DSS_REF}"
 
 API_FOLDER="${REPO_ROOT}/sdk/api"
